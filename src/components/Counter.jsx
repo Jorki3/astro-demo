5a14bf878238b4c776e4729220b44f37bdbbdfc8
dreshenga <dreshenga@gmail.com>
2024-11-21 08:15:19 +0000

    React Counter

---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p className='text-zinc-50'>Has hecho clic {count} veces.</p>
+            <button
+                className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-300"
+                onClick={() => setCount(count + 1)}
+            >
+                ¡Haz clic aquí!
+            </button>
+        </div>
+    );
+}

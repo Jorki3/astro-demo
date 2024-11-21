283aa0af34188bbbc69b66edbaf9830d79969648
dreshenga <dreshenga@gmail.com>
2024-11-21 08:42:02 +0000

    UniqueFeatures

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,7 +5,8 @@ export default function Counter() {
 
     return (
         <div>
-            <p className='text-zinc-50'>Has hecho clic {count} veces.</p>
+            <p className='text-zinc-50 text-center'>Has hecho clic {count} veces.</p>
+
             <button
                 className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50 transition duration-300"
                 onClick={() => setCount(count + 1)}
